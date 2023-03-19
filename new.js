9fbb2f71db0282ce5ba7f492cc8ddebd59a98054
SwastikNanda <84078743+SwastikNanda@users.noreply.github.com>
2023-03-19 19:57:42 +0530

    Add files via upload

--- new.js
+++ new.js
@@ -1,8 +1,4 @@
 const url="https://opentdb.com/api.php?amount=10";
-// let data=fetch(url);
-// console.log(data);
-
-// let da;
  // API for get requests
  let fetchRes = fetch(url);
     // fetchRes is the promise to resolve
@@ -11,11 +7,7 @@ const url="https://opentdb.com/api.php?amount=10";
         res.json()).then(d => {
             console.log(d);
             let da=JSON.stringify(d);
-            document.querySelector("#p1").innerHTML=da; 
+            // document.querySelector("#ques").innerHTML=da; 
             let dada=JSON.parse(da);
-            document.querySelector("#p1").innerHTML=dada.results[0].question;      
+            document.querySelector("#ques").innerHTML=dada.results[0].question;      
         })
-
-// let dada=JSON.parse(da);
-// document.querySelector("#p1").innerHTML=dada[0].question;
-        
